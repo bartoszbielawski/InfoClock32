49f0ac0f31395bf26612418b7b2ddbd73b763c00
Bartosz Bielawski <bartosz.bielawski@gmail.com>
2018-06-08 00:10:29 +0200

    Preparing configuration editor :)

--- src/webServer.cpp
+++ src/webServer.cpp
@@ -15,22 +15,63 @@ AsyncWebServer& getWebServer()
     return webServer;
 }
 
+void handleRoot(AsyncWebServerRequest *request)
+{
+    request->send(SPIFFS, "/templates/index.html", String(), false);
+}
+
+void handleConfigEditor(AsyncWebServerRequest* request)
+{
+    if (not request->authenticate("admin", "admin"))
+    {
+        request->requestAuthentication();
+    }
+   
+    auto file = SPIFFS.open("/config.txt", "r");
+    auto contents = file.readString();
+
+    Serial.println(contents);
+
+    file.close();
+
+    request->send(SPIFFS, "/templates/configEditor.html", "text/html", false, [&contents](const String& k) {return contents;});
+}
+
+void handleConfigSave(AsyncWebServerRequest* request)
+{
+    if (not request->authenticate("admin", "admin"))
+    {
+        request->requestAuthentication();
+    }
+
+    if (!request->hasArg(F("content")))
+        request->redirect("/");
+
+    const String content = request->arg("content");
+
+    auto file = SPIFFS.open("/config.txt", "w");
+    file.print(content);
+    file.close();
+
+    Serial.println(content);
+
+    request->redirect("/");
+}
+
 void configureWebServer()
 {
     Serial.println("Configuring server...");
     webServer.onNotFound(
          [](AsyncWebServerRequest *request)
          {
-             request->send(404, "plain/text", "Not found :(");
+             request->send(404);
          });
 
-    webServer.serveStatic("/files/", SPIFFS, "/");
-    webServer.on("/", HTTP_GET, 
-        [](AsyncWebServerRequest *request)
-        {
-            request->send(200, "text/plain", "I'm working!");
-        }
-    );
+    webServer.serveStatic("/static_files/", SPIFFS, "/static_files/");
+    webServer.on("/", handleRoot);
+    webServer.on("/configEditor", handleConfigEditor);
+    webServer.on("/configSave", handleConfigSave);
     webServer.begin();
     Serial.println("Done...");
 }
+
